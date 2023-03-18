fcf62cd6f90ebcdb52512b76261ece5791e3968a
kuzma7 <122277574+kuzma7@users.noreply.github.com>
2023-03-18 14:03:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+    int right = size - 1;
+    int count = 0;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid - 1;
+            while (i >= left && arr[i] == value) {
+                count++;
+                i--;
+            }
+            i = mid + 1;
+            while (i <= right && arr[i] == value) {
+                count++;
+                i++;
+            }
+            return count;
+        } else if (arr[mid] > value) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return count;
 }
